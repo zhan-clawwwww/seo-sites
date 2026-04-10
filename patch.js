fa8893ba7df2c15f3c6cb448a42fa4c64797e8b0
wangzhanpeng <wangzhanpeng@sdibc.cn>
2026-04-10 12:28:31 +0800

    ook

---
+++ patch.js
@@ -0,0 +1,9 @@
+﻿function renderQuestions() {
+    questionList.innerHTML = '';
+    const visibleQuestions = getVisibleQuestions();
+    visibleQuestions.forEach((q, i) => {
+        const card = renderQuestionCard(q, i);
+        questionList.appendChild(card);
+    });
+    updateProgress();
+}
